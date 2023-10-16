867ee8df29c7a8f369cb62d7d335c03b72a2e131
danielcaag <daniel.canizares@estudiantat.upc.edu>
2023-10-16 12:13:30 +0200

    Definició de funcions entrada alfabet

--- src/domain/Alfabet.java
+++ src/domain/Alfabet.java
@@ -1,4 +1,19 @@
 package src.domain;
 public class Alfabet {
     public String nom;
+
+    // Gestionar alfabet
+    public void readAlfabet() { //a definir quin tipus de dades es vol rebre com a parametre
+
+    }
+
+    // Gestionar text d'entrada
+    public void readText(String text) {
+
+    }
+
+    // Gestionar llistes de paraules
+    public void readWords() { //conjunt de paraules amb la seva probabilitat
+
+    }
 }
