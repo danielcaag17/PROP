f81cf42c3f9427b6c666c2ff10681eaf64ae40b8
Pau Rambla <pau.ramnbla@gmail.com>
2023-10-22 12:36:59 +0200

    1a Implementació del Main i CtrlPresentació

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 package src;
+
+import java.io.FileNotFoundException;
+
+import src.presentation.CtrlPresentacio;
+
 public class Main {
 
-    public static void main (String[] args) {
-        System.out.println("Hello World");
+    public static void main (String[] args) throws FileNotFoundException {
+        CtrlPresentacio c = new CtrlPresentacio();
     }
 }
---
+++ src/presentation/CtrlPresentacio.java
@@ -0,0 +1,18 @@
+package src.presentation;
+import java.io.FileNotFoundException;
+
+import src.domain.controllers.CtrlDomini;
+
+public class CtrlPresentacio {
+    private CtrlDomini controlDomini;
+
+    public CtrlPresentacio() throws FileNotFoundException {
+        // Aquí s'ha d'iniciar Factoria, Controlador domini, la visualització (per a la primera entrega potser per terminal)
+        /*
+        FactoriaCtrl fc = FactoriaCtrl.getInstance();
+        fc.CrearControladorDomini();
+        controlDomini = fc.getCtrlDomini();
+        // ... + terminal + altres
+        */
+    }
+}
