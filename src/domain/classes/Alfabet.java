967edeedbe83e796730933f754b2a772f41dca2e
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-10-27 15:52:02 +0200

    Mitja implementació readText

--- src/domain/classes/Alfabet.java
+++ src/domain/classes/Alfabet.java
@@ -1,7 +1,13 @@
 package src.domain.classes;
 
+import java.util.Map;
+
 public class Alfabet {
-    private String nom;
+    private String nom;                                             // clau primària
+    private Map<char, float> characters = new Map<char, float>();   // cada caràcter de l'alfabet amb la seva corresponent probabilitat
+    private float[][] x = new float[][];                    // a veure quina Estructura de Dades utilitzar + quin nom
+    int midaAlfabet;       // potser estaria be saber quants caracters diferents té labecedari per aixi poder inicialitzar les dades amb l'espai que necessiten
+
     //vector[]<char> = new vector[mida alfabet]
     //vector[]<float> = new vector[mida alfabet]
     //matrix[][]<float> = new matrix[mida alfabet] ----> probabilitat dondada una lletra sigui la seguent
@@ -12,25 +18,35 @@ public class Alfabet {
 
     }
 
-    // Gestionar text d'entrada
+    // Gestionar un text d'entrada
     public void readText(String text) {
         int length = text.length();
         for (int i = 0; i < length; i++) {
-            if (!included(text.charAt(i))) {
-                //vector de lletras push
-            }
-            //sumar una aparicio de la lletra
-            if (i < length - 1) {
-                //veure la lletra seguent i sumar a la aprobabilitat
+            char c = text.charAt(i);                    // obtenir la lletra del text
+            if (! included(c))                          // comprovar que la lletra no s'ha vist encara
+                characters.put(c, 0);                   // crear una nova entrada amb la lletra c i 0 aparicions
+            float a = characters.get(text.charAt(i));   // obtenir el nombre d'aparicions de la lletra
+            characters.replace(text.charAt(i), a+1);    // sumar un a les aparicions de la lletra c
+            if (i < length - 1) {                       // veure la lletra seguent
+                char next = text.charAt(i+1);
+                x[c][next]++;                      // molt molt pseudocodi
             }
         }
 
-        /*for (i in vector length)
-         * dividir la matrix duna fila pel valor que conte el vector que conta el nombre
-         * de vegades que apareix la lletra
-         * 
-         * dividr tot el vector de les aparicions de la lletra per length
-         */
+        Iterator it = characters.entrySet().iterator();
+        while (it.hasNext()) {
+            float a = characters.get(text.charAt(i));   // obtenir el nombre d'aparicions de la lletra
+            float probabilitat = a / length;            // obtenir la probabilitat
+            characters.replace(text.charAt(i), a);      // associar la lletra amb la seva probabilitat
+
+            //for (i = 0; i < mida de alfabet; i++) {
+                //x[it.key][i] /= a;
+
+                //dividir la matrix duna fila pel valor que conte el vector que conta el nombre
+                //de vegades que apareix la lletra 
+            //}
+            it.next();
+        }
     }
 
     // Gestionar llistes de paraules
@@ -40,6 +56,6 @@ public class Alfabet {
 
     // aquesta lletra existeix en el vector
     private boolean included(char c) {
-        return true;
+        return characters.containsKey(c);               // comprovar si el map te la clau c
     }
 }
