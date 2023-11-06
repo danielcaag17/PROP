ce45991423d92b062d9b61c4a412be0703a1b4e3
Pau Rambla <pau.ramnbla@gmail.com>
2023-11-07 00:42:36 +0100

    Afegit atribut 'mida'

--- src/domain/classes/Layout.java
+++ src/domain/classes/Layout.java
@@ -2,6 +2,7 @@ package src.domain.classes;
 
 public class Layout {
     private int id;
+    private int mida; // Indica la mida del layout, número de posicions disponibles
     // atributs necessaris
 
     // metodes necessaris
