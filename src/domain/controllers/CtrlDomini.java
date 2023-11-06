e87399764f1cd670d22abf8d066bba1753ae486d
Pau Rambla <pau.ramnbla@gmail.com>
2023-11-07 00:44:41 +0100

    Inicialització d'instàncies i var. implemented

--- src/domain/controllers/CtrlDomini.java
+++ src/domain/controllers/CtrlDomini.java
@@ -1,13 +1,22 @@
 package src.domain.controllers;
 
+import java.util.*;
+
 import src.data.*;
-import src.domain.*;
+import src.domain.classes.*;
 import src.exceptions.*;
 
 public class CtrlDomini {
     /*
      * Instàncies de les classes del model
      */
+    private CtrlAlfabetFile ctrlAlfabetFile;
+    private Generador Generador;
+    // private String strategy;
+    private HashMap<String, Teclat> Teclats; // Cjt de Teclats, on String és el nom del Teclat
+    private HashMap<String, Alfabet> Alfabets; // Cjt d'Alfabets, on String és el nom de l'Alfabet
+    private HashMap<String, Layout> Layouts; // Cjt de Layouts, on String és l'id del Layout
+
     private static CtrlDomini singletonDomini;
 
     // Pre:
@@ -21,8 +30,17 @@ public class CtrlDomini {
         return singletonDomini;
     }
 
+    // Pre:
+    // Post: s'han inicialitzat les instàncies del model i variables del CtrlDomini
     public void init() {
-        // Inicialització de les diverses instàncies del model
+//      ctrlAlfabetFile = CtrlAlfabetFile.getInstance();
+        Teclats = new HashMap<String, Teclat>();
+        Alfabets = new HashMap<String, Alfabet>();
+        Layouts = new HashMap<String, Layout>();
+//      strategy = "QuadraticAssignmentProblem"
     }
 
+    /* Funcions de Transacció i de Domini */
+
+    
 }
