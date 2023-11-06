ca962b0fe9b2681856e9fcfe2b191a6f5a274307
Pau Rambla <pau.ramnbla@gmail.com>
2023-11-06 20:43:21 +0100

    Implementació primitiva d'inicialit. model i ctrls

--- src/domain/controllers/CtrlDomini.java
+++ src/domain/controllers/CtrlDomini.java
@@ -5,5 +5,24 @@ import src.domain.*;
 import src.exceptions.*;
 
 public class CtrlDomini {
-    
+    /*
+     * Instàncies de les classes del model
+     */
+    private static CtrlDomini singletonDomini;
+
+    // Pre:
+    // Post: s'ha creat una instància de controlador de domini
+    public CtrlDomini() {
+        init();
+    }
+
+    public static CtrlDomini getInstance() {
+        if(singletonDomini == null) singletonDomini = new CtrlDomini();
+        return singletonDomini;
+    }
+
+    public void init() {
+        // Inicialització de les diverses instàncies del model
+    }
+
 }
--- src/presentation/CtrlPresentacio.java
+++ src/presentation/CtrlPresentacio.java
@@ -1,18 +1,19 @@
 package src.presentation;
 import java.io.FileNotFoundException;
 
-import src.domain.controllers.CtrlDomini;
+import src.domain.controllers.*;
 
 public class CtrlPresentacio {
+    private IOterminal ioTerminal;
     private CtrlDomini controlDomini;
 
     public CtrlPresentacio() throws FileNotFoundException {
         // Aquí s'ha d'iniciar Factoria, Controlador domini, la visualització (per a la primera entrega potser per terminal)
-        /*
-        FactoriaCtrl fc = FactoriaCtrl.getInstance();
-        fc.CrearControladorDomini();
-        controlDomini = fc.getCtrlDomini();
-        // ... + terminal + altres
-        */
+        Factoria factoriaCtrl = Factoria.getInstance();
+        factoriaCtrl.crearControladorDomini();
+        controlDomini = factoriaCtrl.getCtrlDomini();
+        ioTerminal = new IOterminal(this);
+        // Inicialització de ctrlDomini
+        // Inicialització de terminal
     }
 }
