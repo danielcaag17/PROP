8c8d659093d19d6e0a7a4a0a4469dd79baa4c97a
Pau Rambla <pau.ramnbla@gmail.com>
2023-11-06 22:58:18 +0100

    Idees sobre IO comentades

--- src/presentation/IOterminal.java
+++ src/presentation/IOterminal.java
@@ -15,4 +15,15 @@ public class IOterminal {
     public void inicialitza() {
         // Codi inicialització terminal (welcome msg)
     }
+
+    /* 
+     * IMPORTANT PENSAR COMANDES D'INPUT
+     * - tipus: nou_alfabet nou_teclat borrar_teclat etc.
+     * 
+     * Interacció pensada per a la creació de nou alfabet
+     * - Preguntar pel nom que es vol donar al alfabet
+     * - Preguntar pel tipus de dades que s'introdueixen (llista de freqüències o text)
+     * - Demanar el path al fitxer on estan guardades
+     * - L'IOterminal comprova que el fitxer existeix i si no torna a demanar el path.
+     */
 }
