b8a5b95581395b6414491f5026c9cca2f9d51134
Pau Rambla <pau.ramnbla@gmail.com>
2023-11-06 20:43:55 +0100

    Esquelet de la classe IOterminal

---
+++ src/presentation/IOterminal.java
@@ -0,0 +1,18 @@
+package src.presentation;
+
+import java.io.*;
+import java.util.*;
+
+public class IOterminal {
+    private CtrlPresentacio ctrlPresentacio;
+    private Scanner s;
+
+    public IOterminal(CtrlPresentacio cp) {
+        ctrlPresentacio = cp;
+        // s = new Scanner(System.in);
+    }
+
+    public void inicialitza() {
+        // Codi inicialització terminal (welcome msg)
+    }
+}
