1e28ee150464d95d8cd585892cf74d964760205a
jotal-fib <jordi.otal@estudiantat.upc.edu>
2023-11-07 13:58:35 +0100

    added comments

--- src/domain/classes/QAP.java
+++ src/domain/classes/QAP.java
@@ -1,7 +1,14 @@
 package src.domain.classes;
 
 public class QAP implements Strategy {
+    static float cotaGilmore() {
+        // 1r terme => Calculable
+        // sum(cotas) on:
+        //      cota = [ 1 - (freq(i->j) + freq(j->j))/2] * dist(Pi, Pj)
+
+        // 2n + 3r terme => Hungarian Algorithm
+    }
     public void generarTeclat() {
-        
+        // asd    
     }
 }
