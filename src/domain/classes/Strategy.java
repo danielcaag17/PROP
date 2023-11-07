932e9e88ae8f70bb7cf89e4c17dd5a58383ba714
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-11-07 09:28:37 +0100

    Comentat un import no utilitzat

--- src/domain/classes/Strategy.java
+++ src/domain/classes/Strategy.java
@@ -1,6 +1,6 @@
 package src.domain.classes;
 
-import src.domain.controllers.*;
+// import src.domain.controllers.*;
 
 public interface Strategy {
     public char[][] generarTeclat(Layout L, Alfabet A); // a definir quina Estructura retorna
