e59046f3ec187740bb7408490baa5c12ecee7388
Pau Rambla <pau.ramnbla@gmail.com>
2023-11-08 00:08:49 +0100

    Esquelet inicial Layout

--- src/domain/classes/Layout.java
+++ src/domain/classes/Layout.java
@@ -3,7 +3,26 @@ package src.domain.classes;
 public class Layout {
     private int id;
     private int mida; // Indica la mida del layout, número de posicions disponibles
+    private int[][] distanciaPosicions; // Indica la distància entre les "mida" posicions
+
+    // Creadora
+    public Layout(int i, int m) {
+        id = i;
+        mida = m;
+    }
+
     // atributs necessaris
 
     // metodes necessaris
+    public int getId() {
+        return id;
+    }
+
+    public int getMida() {
+        return mida;
+    }
+
+    public int[][] getDistancies() {
+        return distanciaPosicions;
+    }
 }
