e09aa7b3853200fce9f38d63cae483ad866a1226
Pau Rambla <pau.ramnbla@gmail.com>
2023-11-08 14:16:30 +0100

    Noves funcions a CtrlDomini

--- src/domain/controllers/CtrlDomini.java
+++ src/domain/controllers/CtrlDomini.java
@@ -17,7 +17,6 @@ public class CtrlDomini {
     private HashMap<String, Teclat> Teclats; // Cjt de Teclats, on String és el nom del Teclat
     private HashMap<String, Alfabet> Alfabets; // Cjt d'Alfabets, on String és el nom de l'Alfabet
     private HashMap<String, Layout> Layouts; // Cjt de Layouts, on String és l'id del Layout
-
     private static CtrlDomini singletonDomini;
 
     // Pre:
@@ -43,6 +42,106 @@ public class CtrlDomini {
     }
 
     /* Funcions de Transacció i de Domini */
+
+    // Crea les instàncies de Layout de les mides predefinides.
+    private void inicialitzarLayoutsBase() {
+        for (int i = 0; i<midesInicials.length; i++) {
+            Layout l = new Layout(i+1, midesInicials[i]);
+            String key = String.valueOf(midesInicials[i]);
+            Layouts.put(key, l);
+        }
+    }
+    
+    /**
+     *  @return Retorna el teclat amb nom nt.
+     */ 
+    public Teclat getTeclat(String nt) {
+        return Teclats.get(nt);
+    }
+
+    /** 
+     * @return Retorna l'alfabet amb nom na
+     */
+    public Alfabet getAlfabet(String na) {
+        return Alfabets.get(na);
+    }
+
+    /**
+     * @return Retorna el Layout amb id=idL
+     */
+    public Layout getLayout(String idL) {
+        return Layouts.get(idL);
+    }
+
+    /**
+     * Pre: el teclat amb nom nt no existeix.
+     * Post: el teclat amb nom nt s'ha creat i associat amb l'alfabet amb nom na i el Layout amb id idL
+     * 
+     * @param nt: nom del teclat 
+     * @param na: nom del alfabet del teclat
+     * @param idL: id del Layout del teclat
+     * 
+     * @throws TeclatJaExisteix si existeix una instància Teclat amb nom nt.
+     * @throws MidesDiferents si la mida del Alfabet amb nom na i el Layout amb id idL són diferents. 
+     */
+    public void crearNouTeclat(String nt, String na, String idL) throws TeclatJaExisteix, MidesDiferents {
+        if (Teclats.get(nt) != null) throw new TeclatJaExisteix(nt);
+        // excepció per a existència de na ?
+        // excepció per a existència de idL ?
+        Alfabet a = Alfabets.get(na);
+        Layout l = Layouts.get(idL);
+        if (a.getMida() != l.getMida()) throw new MidesDiferents(a.getMida(), l.getMida());
+        Teclat t = new Teclat(nt, l, a, Generador); // IMPORTANT QUE CREADORA TECLAT SIGUI PUBLIC
+        t.crearTeclat(); // potser passar strategy?
+        Teclats.put(nt, t);
+    }
+
+    /**
+     * Pre: el teclat amb nom nt existeix, els canvis són vàlids.
+     * Post: el teclat amb nom nt s'ha modificat amb els canvis aplicats.
+     * 
+     * @param nt: nom del teclat 
+     * @param canvis: canvis que es volen aplicar al teclat
+     * @return Teclat que ha sigut modificat. 
+     * 
+     * @throws TeclatNoExisteix si no existeix una insància Teclat amb nom nt.
+     */
+    public Teclat modificarTeclat(String nt, Map<Character, Character> canvis) throws TeclatNoExisteix {
+        if (Teclats.get(nt) == null) throw new TeclatNoExisteix(nt);
+        Teclat t = getTeclat(nt);
+        for (Character c : canvis.keySet()) {
+            t.modificarTeclat(c, canvis.get(c)); 
+        }
+        Teclats.replace(nt, t);
+        return t;
+    }
+
+    /**
+     * Pre: el teclat amb nom nt existeix
+     * 
+     * @param nt: nom del teclat
+     * @return Teclat amb nom nt.
+     * 
+     * @throws TeclatNoExisteix si no existeix una insància Teclat amb nom nt.
+     */
+    public Teclat visualitzarTeclat(String nt) throws TeclatNoExisteix {
+        if (Teclats.get(nt) == null) throw new TeclatNoExisteix(nt);
+        return getTeclat(nt);
+    }
+
+    /**
+     * Pre: el teclat amb nom nt existeix
+     * Post: el teclat amb nom nt s'ha esborrat
+     * 
+     * @param nt: nom del teclat
+     * 
+     * @throws TeclatNoExisteix si no existeix una insància Teclat amb nom nt.
+     */
+    public void esborrarTeclat(String nt) throws TeclatNoExisteix {
+        if (Teclats.get(nt) == null) throw new TeclatNoExisteix(nt);
+        Teclats.remove(nt);
+    }
+
     /*
      * - getters
      * - inicialitzarLayoutsBase()
@@ -55,47 +154,4 @@ public class CtrlDomini {
      * - visualitzarLayout(idL)
      */
 
-    // Crea les instàncies de Layout de les mides predefinides.
-    private void inicialitzarLayoutsBase() {
-        for (int i = 0; i<midesInicials.length; i++) {
-            Layout l = new Layout(i+1, midesInicials[i]);
-            String key = String.valueOf(midesInicials[i]);
-            Layouts.put(key, l);
-        }
-    }
-    
-    // Retorna el teclat amb nom nt
-    public Teclat getTeclat(String nt) {
-        return Teclats.get(nt);
-    }
-
-    // Retorna l'alfabet amb nom na
-    public Alfabet getAlfabet(String na) {
-        return Alfabets.get(na);
-    }
-
-    // Retorna el Layout amb id=idL
-    public Layout getLayout(String idL) {
-        return Layouts.get(idL);
-    }
-
-    /*
-     * nt: nom del teclat 
-     * na: nom del alfabet del teclat
-     * idL: id del Layout del teclat
-     * Pre: el teclat amb nom nt no existeix
-     * Post: el teclat amb nom nt s'ha creat i associat amb l'alfabet amb nom na i el Layout amb id idL
-     * Excepcions: 
-     * - El teclat nt existeix
-     * - La mida del alfabet na i el layout idL és diferent
-     */
-    public void crearNouTeclat(String nt, String na, String idL) throws TeclatJaExisteix, MidesDiferents {
-        Alfabet a = Alfabets.get(na);
-        Layout l = Layouts.get(idL);
-        if (a.getMida() != l.getMida()) throw new MidesDiferents(a.getMida(), l.getMida());
-        if (Teclats.get(nt) != null) throw new TeclatJaExisteix(nt);
-        Teclat t = new Teclat(nt, l, a, Generador); // IMPORTANT QUE CREADORA TECLAT SIGUI PUBLIC
-        t.generarTeclat(); // potser passar strategy?
-        Teclats.put(nt, t);
-    }
 }
