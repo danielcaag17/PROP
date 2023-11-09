5adde65d66eabda0dd6b8415bdbab626bd66b64a
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-11-09 20:32:43 +0100

    Canvi estructura dades de Teclat

--- src/domain/classes/Generador.java
+++ src/domain/classes/Generador.java
@@ -1,9 +1,11 @@
 package src.domain.classes;
 
+import java.util.Map;
+
 public class Generador { // segons el patró estrategia, la classe no ha de ser abstracta
     private Strategy S;
 
-    public char[][] generarTeclat(Layout L, Alfabet A) {
+    public Map<Character, Integer> generarTeclat(Layout L, Alfabet A) {
         return S.generarTeclat(L, A);   // a definir quina estructura retorna
     }
 }
--- src/domain/classes/Strategy.java
+++ src/domain/classes/Strategy.java
@@ -1,7 +1,9 @@
 package src.domain.classes;
 
+import java.util.Map;
+
 // import src.domain.controllers.*;
 
 public interface Strategy {
-    public char[][] generarTeclat(Layout L, Alfabet A); // a definir quina Estructura retorna
+    public Map<Character, Integer> generarTeclat(Layout L, Alfabet A); // a definir quina Estructura retorna
 }
