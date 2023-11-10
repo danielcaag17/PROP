6be522406226177a5b39dedbfb5e2e133316b434
danielcaag <daniel.canizares@estudiantat.upc.edu>
2023-11-10 12:21:54 +0100

    funcio creadora Generador

--- src/domain/classes/Generador.java
+++ src/domain/classes/Generador.java
@@ -5,6 +5,10 @@ import java.util.Map;
 public class Generador { // segons el patró estrategia, la classe no ha de ser abstracta
     private Strategy S;
 
+    public Generador () {
+        // res
+    }
+
     public Map<Character, Integer> generarTeclat(Layout L, Alfabet A) {
         return S.generarTeclat(L, A);   // a definir quina estructura retorna
     }
