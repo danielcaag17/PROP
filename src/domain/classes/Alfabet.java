53bd3f9e07025e59413506d73952b4d6c0a50787
danielcaag <daniel.canizares@estudiantat.upc.edu>
2023-11-10 13:24:46 +0100

    readWords implementat en alfabet

--- src/domain/classes/Alfabet.java
+++ src/domain/classes/Alfabet.java
@@ -2,12 +2,9 @@ package src.domain.classes;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Map;       // mirar quin es import que sha de fer servir
 import java.util.Scanner;
-import java.util.Map;       // mirar quin es import que sha de fer servir
-import java.util.Set;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class Alfabet {
     private String nom;                                             // clau primària
@@ -15,12 +12,9 @@ public class Alfabet {
     private float[][] x;                    // a veure quina Estructura de Dades utilitzar + quin nom
     int size;       // potser estaria be saber quants caracters diferents té labecedari per aixi poder inicialitzar les dades amb l'espai que necessiten
 
-    //vector[]<char> = new vector[mida alfabet]
-    //vector[]<float> = new vector[mida alfabet]
-    //matrix[][]<float> = new matrix[mida alfabet] ----> probabilitat dondada una lletra sigui la seguent
-
     public Alfabet (String nom) {
         this.nom = nom;
+        size = 0;
     }
 
     // Gestionar un text d'entrada
@@ -33,19 +27,23 @@ public class Alfabet {
 
      // Gestionar llistes de paraules
     public void readWords(String path) { //conjunt de paraules amb la seva probabilitat
-        Iterator it = words.entrySet().iterator();      //veure si aixo esta ben fet
-        while (it.hasNext()) {
-            processCharacters(it);
-            it.next();
+        HashMap<String, Float> words = getWords(path);
+        int totalLenght = 0;
+        for (String s : words.keySet()) {
+            int lenght = s.length();
+            processCharacters(s, lenght);
+            totalLenght += lenght;
         }
-        calculateProbabilities();       // falta veure de quina manera es calculen les probabilitats de la propia paraula
+        calculateProbabilities(totalLenght);
     }
 
     private void processCharacters(String text, int length) { //return lenght (?) per calculateProbabilities
         for (int i = 0; i < length; i++) {
             Character c = text.charAt(i);                    // obtenir la lletra del text
-            if (! included(c))                          // comprovar que la lletra no s'ha vist encara
+            if (! included(c)) {                         // comprovar que la lletra no s'ha vist encara
+                size++;
                 characters.put(c, 0.f);;                  // crear una nova entrada amb la lletra c i 0 aparicions
+            }
             float a = characters.get(text.charAt(i));   // obtenir el nombre d'aparicions de la lletra
             characters.replace(text.charAt(i), a+1);    // sumar un a les aparicions de la lletra c
             if (i < length - 1) {                       // veure la lletra seguent
@@ -62,9 +60,9 @@ public class Alfabet {
             float a = characters.get(c);   // obtenir el nombre d'aparicions de la lletra
             float probabilitat = a / length;            // obtenir la probabilitat
             float nAparicions = characters.get(c);
-            characters.replace(c, a);      // associar la lletra amb la seva probabilitat
+            characters.replace(c, probabilitat);      // associar la lletra amb la seva probabilitat
 
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) { //size ha de ser de la  mateixa mida que la matriu
                 x[c][i] /= nAparicions;     //dividir la matrix duna fila pel valor que conte el vector que conta el nombre
                                             //de vegades que apareix la lletra 
             }
@@ -77,7 +75,7 @@ public class Alfabet {
             File file = new File(path);
             Scanner myReader = new Scanner(file);
             while (myReader.hasNextLine()) {
-            text += myReader.nextLine();
+                text += myReader.nextLine();
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -86,6 +84,29 @@ public class Alfabet {
             e.printStackTrace();
         }
         return text;
+    }
+
+    private HashMap<String, Float> getWords (String path) {
+        HashMap<String, Float> words = new HashMap<>();
+        try {
+            File file = new File(path);
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String input = myReader.nextLine();
+                String[] dividit = input.split(" ");
+                String word = dividit[0];
+                Float probabilitat = Float.parseFloat(dividit[1]);
+                if (! words.containsKey(word)) {             // comprovar que la lletra no s'ha vist encara
+                    words.put(word, probabilitat);;                  // crear una nova entrada amb la lletra c i 0 aparicions
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            // No passarà mai perque el path ja es comprova a ctrlDomini
+            System.err.println("PathIncorrecte");
+            e.printStackTrace();
+        }        
+        return words;
     }
 
     // aquesta lletra existeix en el vector
