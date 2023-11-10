3ee5050d9622a77aac1408b990f9a0b670fd10c8
danielcaag <daniel.canizares@estudiantat.upc.edu>
2023-11-10 13:45:10 +0100

    driver de la classe Alfabet

--- src/drivers/DriverAlfabet.java
+++ src/drivers/DriverAlfabet.java
@@ -1,5 +1,11 @@
 package src.drivers;
 
+import src.domain.classes.Alfabet;
+
 public class DriverAlfabet {
-    
+    private Alfabet a = new Alfabet(null);
+
+    public void getNomTest(){
+        System.out.println("El nom de l'Alfabet es "+a.getNom()+"\n");
+    }
 }
