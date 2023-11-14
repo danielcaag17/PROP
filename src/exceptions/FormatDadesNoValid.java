a31b7e36178d47d587393ad31d977ed4770c89d7
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-11-14 20:27:05 +0100

    Creada Excepció FormatDadesNoValid

---
+++ src/exceptions/FormatDadesNoValid.java
@@ -0,0 +1,13 @@
+package src.exceptions;
+
+public class FormatDadesNoValid extends Excepcions {
+    public String getTipus() {
+        return "FormatDadesNoValid";
+    }
+    public FormatDadesNoValid() {
+        super("El format de les dades del fitxer introduït no es correspon amb el seu tipus.");
+    }
+    public FormatDadesNoValid(String s) {
+        super("El format de les dades del fitxer "+s+" introduït no es correspon amb el seu tipus.");
+    }
+}
