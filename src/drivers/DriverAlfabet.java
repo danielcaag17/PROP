356839f086c51cdca4d0df0e0d8af1027a0b6284
danielcaag <daniel.canizares@estudiantat.upc.edu>
2023-11-17 12:45:29 +0100

    Principi driverAlfabet

--- src/drivers/DriverAlfabet.java
+++ src/drivers/DriverAlfabet.java
@@ -3,9 +3,41 @@ package src.drivers;
 import src.domain.classes.Alfabet;
 
 public class DriverAlfabet {
-    private Alfabet a = new Alfabet(null);
+    private static Alfabet a1, a2, a3;
+    private String pathText1 = "../../test/Text1.txt";      // Text en anglès
+    private String pathText2 = "../../test/Text2.txt";      // Text en català
+    private String pathText3 = "../../test/Text3.txt";      // Text en castellà
+    private String pathWords1 = "../../test/Words1.txt";    // Frecuencies en anglès
+    private String pathWords2 = "../../test/Words2.txt";    // Frecuencies en català
+    private String pathWords3 = "../../test/Words3.txt";    // Frecuencies en castellà
 
-    public void getNomTest(){
-        System.out.println("El nom de l'Alfabet es "+a.getNom()+"\n");
+
+    public static void main (String[] args) {
+        a1 = new Alfabet(null);
+        a2 = new Alfabet("Alfabet A2");
+        a3 = new Alfabet("Alfabet A3")
+
+        //-------------------------------------------------------------
+        // getNom();
+        System.out.println("El nom de l'Alfabet es "+ a1.getNom()+"\n");
+        System.out.println("El nom de l'Alfabet es "+ a2.getNom()+"\n");
+        System.out.println("El nom de l'Alfabet es "+ a3.getNom()+"\n");
+
+
+        //-------------------------------------------------------------
+        // setNom();
+        System.out.println("Assignar nom a a1 --> A1");
+        a1.setNom("A1");
+        System.out.println("El nom de l'Alfabet es "+ a1.getNom()+"\n");
+
+
+        //-------------------------------------------------------------
+        // readInput();
+        System.out.println("El nom de l'Alfabet es "+ a1.getNom()+"\n");
+        System.out.println("El nom de l'Alfabet es "+ a2.getNom()+"\n");
+
+
+
+
     }
 }
