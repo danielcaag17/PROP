e389947c74f00576b1293ab0ac1a4428e802f75f
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-11-18 16:47:00 +0100

    generador funcions implementades

--- src/domain/classes/Generador.java
+++ src/domain/classes/Generador.java
@@ -11,15 +11,25 @@ public class Generador { // segons el patró estrategia, la classe no ha de ser 
 
     public Map<Character, Integer> generarTeclat(double[][] freqMatrix, double[][] distMatrix, char[] abcdari, Map<Character, Double> freqAbs) {
         double[] freqAbsArray = getFreqAbsolute(abcdari, freqAbs);
-        ArrayList<Integer> resultAlgorithm = S.generarTeclat();   // a definir quina estructura retorna
-        Map<Character, Integer> result = 
+        ArrayList<Integer> resultAlgorithm = S.generarTeclat(freqMatrix, freqAbsArray, distMatrix);   // a definir quina estructura retorna
+        return beautifyResult(resultAlgorithm, abcdari);
     }
 
     private double[] getFreqAbsolute(char[] abcdari, Map<Character, Double> freqAbs) {
-
+        double[] res = new double[abcdari.length];
+        for (int i = 0; i<abcdari.length; i++) {
+            res[i] = freqAbs.get(abcdari[i]);
+        }
+        return res;
     }
 
-    private Map<Character, Integer> beautifyResult(ArrayList<Integer> idsOf, char[] abcdari) {
+    // IMPORTANT: assig[i] conté el index d'abcdari que conté la lletra final en la posició i, tal que i és la id d'una tecla del layout.
+    private Map<Character, Integer> beautifyResult(ArrayList<Integer> assig, char[] abcdari) {
+        Map<Character, Integer> posicioCaracters = new HashMap<Character, Integer>();
+        for (int i = 0; i < assig.size(); i++) {
+            posicioCaracters.put(abcdari[assig.get(i)], i); // afegim l'entrada on extraiem un caràcter i el seu index (id de posició al layout)
+        }
 
+        return posicioCaracters;
     }
 }
