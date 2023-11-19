18754f227a5f5aa4340ebac5f08146014d9339bd
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-11-19 15:36:47 +0100

    Excepcions afegides

--- src/domain/classes/StrategyAlfabet.java
+++ src/domain/classes/StrategyAlfabet.java
@@ -1,5 +1,9 @@
 package src.domain.classes;
 
+import java.io.FileNotFoundException;
+
+import src.exceptions.FormatDadesNoValid;
+
 public interface StrategyAlfabet {
-    public Alfabet read (String path);
+    public Alfabet read (String path)  throws FormatDadesNoValid, FileNotFoundException;
 }
