aa3f1958972397661e1552183c3c7851bb17fe0e
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-11-20 00:24:36 +0100

    small printing errors

--- src/src/domain/controllers/CtrlDomini.java
+++ src/src/domain/controllers/CtrlDomini.java
@@ -110,6 +110,7 @@ public class CtrlDomini {
         for(int i = 0; i < mat.length; i++) {
             s+="\n";
             for(int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 10) s += "0";
                 s += mat[i][j] + " ";
             }
         }
