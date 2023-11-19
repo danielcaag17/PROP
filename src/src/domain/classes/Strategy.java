ec1159e8b0552221368730be75cf4348bcdc2d44
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-11-19 22:32:09 +0100

    Classe Strategy quasi acabada amb comentaris

--- src/src/domain/classes/Strategy.java
+++ src/src/domain/classes/Strategy.java
@@ -2,8 +2,17 @@ package src.domain.classes;
 
 import java.util.*;
 
-// import src.domain.controllers.*;
-
 public interface Strategy {
-    public ArrayList<Integer> generarTeclat(double[][] frequencyMat, double[] frequencyAbsolute, double[][] distanceMat); // a definir quina Estructura retorna
+
+    /**
+     * Pre:
+     * Post: s'han generat les ids per cada lletra
+     * 
+     * @param frequencyMat es la matriu de freqüencies entre lletres del Teclat.
+     * @param frequencyAbsolute es un arry amb les freqüències de cada lletra del Teclat
+     * @param distanceMat es la matriu de les distàncies del Layout
+     * 
+     * @return la id de cada lletra ordenades
+     */
+    public ArrayList<Integer> generarTeclat(double[][] frequencyMat, double[] frequencyAbsolute, double[][] distanceMat);
 }
