f1151ed85dbac0d86a7c8628f5a1df5845d8b9f1
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-11-19 20:17:27 +0100

    make file updated

--- src/make.sh
+++ src/make.sh
@@ -5,9 +5,9 @@ javac --release 11 exceptions/*.java ;
 javac --release 11 presentation/*.java ;
 javac --release 11 Main.java ;
 
-mv domain/classes/*.class ../bin/class/domain/classes ;
-mv domain/controllers/*.class ../bin/class/domain/controllers ;
-mv drivers/*.class ../bin/class/drivers ;
-mv exceptions/*.class ../bin/class/exceptions ;
-mv presentation/*.class ../bin/class/presentation ;
-mv Main.class ../bin/class
+mv domain/classes/*.class ../bin/class/domain/classes/ ;
+mv domain/controllers/*.class ../bin/class/domain/controllers/ ;
+mv drivers/*.class ../bin/class/drivers/ ;
+mv exceptions/*.class ../bin/class/exceptions/ ;
+mv presentation/*.class ../bin/class/presentation/ ;
+mv Main.class ../bin/class/ ;
