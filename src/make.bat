b6cce0c83cc6c8f8a9e0e0e8398e77510b393843
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-11-20 20:20:21 +0100

    Compilació correcta per Windows

--- src/make.bat
+++ src/make.bat
@@ -1,13 +1,13 @@
-javac --release 11 src/domain/classes/*.java ;
-javac --release 11 src/domain/controllers/*.java ;
-javac --release 11 src/drivers/*.java ;
-javac --release 11 src/exceptions/*.java ;
-javac --release 11 src/presentation/*.java ;
-javac --release 11 src/Main.java ;
+javac --release 11 src/domain/classes/*.java 
+javac --release 11 src/domain/controllers/*.java 
+javac --release 11 src/drivers/*.java 
+javac --release 11 src/exceptions/*.java 
+javac --release 11 src/presentation/*.java 
+javac --release 11 src/Main.java 
 
-move src\domain\classes\*.class ..\bin\class\src\domain\classes\ ;
-move src\domain\controllers\*.class ..\bin\class\src\domain\controllers\ ;
-move src\drivers\*.class ..\bin\class\src\drivers\ ;
-move src\exceptions\*.class ..\bin\class\src\exceptions\ ;
-move src\presentation\*.class ..\bin\class\src\presentation\ ;
-move src\Main.class ..\bin\class\src\ ;
+move src\domain\classes\*.class ..\bin\class\src\domain\classes\ 
+move src\domain\controllers\*.class ..\bin\class\src\domain\controllers\ 
+move src\drivers\*.class ..\bin\class\src\drivers\ 
+move src\exceptions\*.class ..\bin\class\src\exceptions\ 
+move src\presentation\*.class ..\bin\class\src\presentation\ 
+move src\Main.class ..\bin\class\src\ 
