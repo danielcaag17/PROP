b7687d82bee3d5b0a2252f19214d5ff3f946c291
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-11-20 21:53:18 +0100

    Classe StrategyAlfabet acabada

--- src/src/domain/classes/StrategyAlfabet.java
+++ src/src/domain/classes/StrategyAlfabet.java
@@ -6,5 +6,14 @@ import src.exceptions.EntradaLlegidaMalament;
 import src.exceptions.FormatDadesNoValid;
 
 public interface StrategyAlfabet {
+
+    /**
+     * Pre:
+     * Post: es retorna l'Alfabet caculat amb l'entrada doanada
+     * 
+     * @param path string que indica on està l'entrada que defineix l'Alfabet
+     * 
+     * @return l'Alfabet resultat de l'entrada llegida
+     */
     public Alfabet read (String path)  throws FormatDadesNoValid, FileNotFoundException, EntradaLlegidaMalament;
 }
