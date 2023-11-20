10af74261a5bdbf7db5df05b37bcc281d1a62ef9
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-11-20 20:26:02 +0100

    DriverAlfabet acabat

--- src/src/drivers/DriverAlfabet.java
+++ src/src/drivers/DriverAlfabet.java
@@ -9,11 +9,15 @@ import src.exceptions.FormatDadesNoValid;
 import src.exceptions.TipusDadesNoValid;
 
 public class DriverAlfabet {
+    // Paths d'exemple per l'opció 1
+    private String text = "./subgrup-prop32.2/test/exemples_input_alfabet/Text1.txt";
+    private String words = "./subgrup-prop32.2/test/exemples_input_alfabet/Words1.txt";
 
     public static void main (String[] args) throws FormatDadesNoValid, TipusDadesNoValid, FileNotFoundException, EntradaLlegidaMalament {
         Alfabet A = new Alfabet("Alfabet de prova");
         Scanner sc = new Scanner(System.in);
         int func = 0;
+        Boolean primeraOpt = false;
         while (func != 6){
             System.out.println("Selecciona una de les funcionalitats:");
             System.out.println("\t1. ReadInput");
@@ -24,7 +28,8 @@ public class DriverAlfabet {
             System.out.println("\t6. Sortir");
             func = sc.nextInt();
             switch(func){
-                case 1: 
+                case 1:
+                    primeraOpt = true; 
                     System.out.println("Indica el format d'entrada:");
                     System.out.println("1 --> text");
                     System.out.println("2 --> llista-paraules");
@@ -40,25 +45,34 @@ public class DriverAlfabet {
                         System.out.println("Indica el path:");
                         String path = sc.nextLine();
                         A.readInput("llista-paraules", path);
+                        System.out.println("Input llegit");
                     }
                     else System.out.println("Opció incorrecte");
                     break;
                 case 2: 
-                    System.out.println(A.getAbecedari());
+                    if (! primeraOpt) System.out.println("Primer has de fer un ReadInput");
+                    else System.out.println(A.getAbecedari());
                     break;
                 case 3: 
-                    double[][] x = A.getFrequencies();
-                    for (int i = 0; i < A.getSize(); i++) {
-                        for (int j = 0; j < A.getSize(); j++) 
-                            System.out.print(Double.toString(x[i][j]) + " ");
-                        System.out.print("\n");
+                    if (! primeraOpt) System.out.println("Primer has de fer un ReadInput");
+                    else {
+                        double[][] x = A.getFrequencies();
+                        for (int i = 0; i < A.getSize(); i++) {
+                            for (int j = 0; j < A.getSize(); j++) {
+                                String s = String.format("%.6f", x[i][j]);
+                                System.out.print(s + " ");
+                            } 
+                            System.out.print("\n");
+                        }
                     }
                     break;
-                case 4: 
-                    System.out.println(A.getCharacter().toString());
+                case 4:
+                    if (! primeraOpt) System.out.println("Primer has de fer un ReadInput"); 
+                    else System.out.println(A.getCharacter().toString());
                     break;
                 case 5: 
-                    System.out.println(A.toString());
+                    if (! primeraOpt) System.out.println("Primer has de fer un ReadInput");
+                    else System.out.println(A.toString());
                     break;
                 case 6:
                     System.out.println("Sortint del driver");
