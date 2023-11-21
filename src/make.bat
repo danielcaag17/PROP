1ef4487a387ca90dfd50bc2fe7cb0daae553bc10
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-11-21 22:47:12 +0100

    Make .bat solucionat

--- src/make.bat
+++ src/make.bat
@@ -1,9 +1,9 @@
 javac --release 11 src/domain/classes/*.java 
 javac --release 11 src/domain/controllers/*.java 
-javac --release 11 -cp .:../lib/junit-4.13.2.jar src/drivers/*.java 
+javac --release 11 -cp .;../lib/junit-4.13.2.jar src/drivers/*.java  
 javac --release 11 src/exceptions/*.java 
 javac --release 11 src/presentation/*.java 
-javac --release 11 src/Main.java 
+javac --release 11 src/Main.java
 
 move src\domain\classes\*.class ..\bin\class\src\domain\classes\ 
 move src\domain\controllers\*.class ..\bin\class\src\domain\controllers\ 
