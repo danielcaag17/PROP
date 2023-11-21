24c54619059bce34a25353e36e298d729845e2c9
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-11-21 11:23:12 +0100

    updated drivers hungarian & complete assignation

--- src/src/drivers/DriverCompleteAssignation.java
+++ src/src/drivers/DriverCompleteAssignation.java
@@ -7,48 +7,39 @@ import src.domain.classes.CompleteAssignation;
 
 public class DriverCompleteAssignation {
     // 4 tests diferents per Hungarian Algorithm
-    private static double[][] test1; // falta assignar valors 
-    private static double[][] test2;
-    private static double[][] test3;
-    private static double[][] test4;
+    private double[][] test1; // falta assignar valors 
+    private double[][] test2;
+    private double[][] test3;
+    private double[][] test4;
     // Els 4 resultats dels tests previs
-    private static int[] result1; // falta assignar valors 
-    private static int[] result2;
-    private static int[] result3;
-    private static int[] result4;
-
-    public static void main(String[] args) {
-        // Execució dels diversos tests
-        test1(); // potser no fan falta al main
-        test2();
-        test3();
-        test4();
-        // (...) se'n poden afegir més
-    }
+    private int[] result1; // falta assignar valors 
+    private int[] result2;
+    private int[] result3;
+    private int[] result4;
 
     @Test
-    public static void test1() {
+    public  void test1() {
         CompleteAssignation ca = new CompleteAssignation(test1);
         int[] result = ca.mostCompleteAssig();
         assertEquals("Test 1: Correcte\n", result, result1);
     }
 
     @Test
-    public static void test2() {
+    public  void test2() {
         CompleteAssignation ca = new CompleteAssignation(test2);
         int[] result = ca.mostCompleteAssig();
         assertEquals("Test 2: Correcte\n", result, result2);
     }
 
     @Test
-    public static void test3() {
+    public  void test3() {
         CompleteAssignation ca = new CompleteAssignation(test3);
         int[] result = ca.mostCompleteAssig();
         assertEquals("Test 3: Correcte\n", result, result3);
     }
 
     @Test
-    public static void test4() {
+    public  void test4() {
         CompleteAssignation ca = new CompleteAssignation(test4);
         int[] result = ca.mostCompleteAssig();
         assertEquals("Test 4: Correcte\n", result, result4);
--- src/src/drivers/DriverHungarian.java
+++ src/src/drivers/DriverHungarian.java
@@ -9,45 +9,36 @@ import src.domain.classes.Hungarian;
 
 public class DriverHungarian {
     // 4 tests diferents per Hungarian Algorithm
-    private static double[][] test1; // falta assignar valors 
-    private static double[][] test2;
-    private static double[][] test3;
-    private static double[][] test4;
+    private double[][] test1; // falta assignar valors 
+    private double[][] test2;
+    private double[][] test3;
+    private double[][] test4;
     // Els 4 resultats dels tests previs
-    private static ArrayList<Integer> result1; // falta assignar valors 
-    private static ArrayList<Integer> result2;
-    private static ArrayList<Integer> result3;
-    private static ArrayList<Integer> result4;
-    
-    public static void main(String[] args) {
-        // Execució dels diversos tests
-        test1(); // potser no fan falta al main
-        test2();
-        test3();
-        test4();
-        // (...) se'n poden afegir més
-    }
+    private ArrayList<Integer> result1; // falta assignar valors 
+    private ArrayList<Integer> result2;
+    private ArrayList<Integer> result3;
+    private ArrayList<Integer> result4;
 
     @Test
-    public static void test1() {
+    public void test1() {
         ArrayList<Integer> result = Hungarian.hungarianAlgorithm(test1);
         assertEquals("Test 1: Correcte\n", result, result1);
     }
 
     @Test
-    public static void test2() {
+    public void test2() {
         ArrayList<Integer> result = Hungarian.hungarianAlgorithm(test2);
         assertEquals("Test 2: Correcte\n", result, result2);
     }
 
     @Test
-    public static void test3() {
+    public void test3() {
         ArrayList<Integer> result = Hungarian.hungarianAlgorithm(test3);
         assertEquals("Test 3: Correcte\n", result, result3);
     }
 
     @Test
-    public static void test4() {
+    public void test4() {
         ArrayList<Integer> result = Hungarian.hungarianAlgorithm(test4);
         assertEquals("Test 4: Correcte\n", result, result4);
     }
