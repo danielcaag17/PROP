f1862fd6c062dfa3769ebd7992b76aa0751b533c
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-11-21 12:55:22 +0100

    compilació de junit funcionant

--- src/make.sh
+++ src/make.sh
@@ -1,6 +1,6 @@
 javac --release 11 src/domain/classes/*.java ;
 javac --release 11 src/domain/controllers/*.java ;
-javac --release 11 src/drivers/*.java ;
+javac --release 11 -cp .:../lib/junit-4.13.2.jar src/drivers/*.java ;
 javac --release 11 src/exceptions/*.java ;
 javac --release 11 src/presentation/*.java ;
 javac --release 11 src/Main.java ;
