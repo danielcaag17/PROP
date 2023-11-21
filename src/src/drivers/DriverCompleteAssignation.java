503c361326a77e45cee4f3f6bfebc9904012e00d
jotal-fib <jordi.otal@estudiantat.upc.edu>
2023-11-21 16:22:47 +0100

    ignore

--- src/src/drivers/DriverCompleteAssignation.java
+++ src/src/drivers/DriverCompleteAssignation.java
@@ -7,23 +7,28 @@ import src.domain.classes.CompleteAssignation;
 
 public class DriverCompleteAssignation {
     // 4 tests diferents per Hungarian Algorithm
-    private double[][] test1; // falta assignar valors 
+    private double[][] test1;
+/*
     private double[][] test2;
     private double[][] test3;
     private double[][] test4;
+*/
     // Els 4 resultats dels tests previs
-    private int[] result1; // falta assignar valors 
+    private int[] result1; 
+/*
     private int[] result2;
     private int[] result3;
     private int[] result4;
+*/
 
     @Test
-    public  void test1() {
+    public void test1() {
         CompleteAssignation ca = new CompleteAssignation(test1);
         int[] result = ca.mostCompleteAssig();
         assertEquals("Test 1: Correcte\n", result, result1);
     }
 
+/*
     @Test
     public  void test2() {
         CompleteAssignation ca = new CompleteAssignation(test2);
@@ -44,4 +49,5 @@ public class DriverCompleteAssignation {
         int[] result = ca.mostCompleteAssig();
         assertEquals("Test 4: Correcte\n", result, result4);
     }
+*/
 }
--- src/src/drivers/DriverHungarian.java
+++ src/src/drivers/DriverHungarian.java
@@ -9,15 +9,22 @@ import src.domain.classes.Hungarian;
 
 public class DriverHungarian {
     // 4 tests diferents per Hungarian Algorithm
-    private double[][] test1; // falta assignar valors 
+    private double[][] test1 = {{21, 34, 31, 43},
+                                {20, 35, 32, 44},
+                                {20, 34, 33, 45},
+                                {21, 34, 31, 43}};
+/*
     private double[][] test2;
     private double[][] test3;
     private double[][] test4;
+*/
     // Els 4 resultats dels tests previs
-    private ArrayList<Integer> result1; // falta assignar valors 
+    private ArrayList<Integer> result1 = new ArrayList<>(Arrays.asList(2, 0, 1, 3)); 
+/*
     private ArrayList<Integer> result2;
     private ArrayList<Integer> result3;
     private ArrayList<Integer> result4;
+*/
 
     @Test
     public void test1() {
@@ -25,6 +32,7 @@ public class DriverHungarian {
         assertEquals("Test 1: Correcte\n", result, result1);
     }
 
+/*
     @Test
     public void test2() {
         ArrayList<Integer> result = Hungarian.hungarianAlgorithm(test2);
@@ -42,5 +50,5 @@ public class DriverHungarian {
         ArrayList<Integer> result = Hungarian.hungarianAlgorithm(test4);
         assertEquals("Test 4: Correcte\n", result, result4);
     }
-
+*/
 }
