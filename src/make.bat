7a26196432b568bcb7e86fb6399a631a1c8d72b8
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-12-16 23:28:08 +0100

    added json library and makefile updated

--- src/make.bat
+++ src/make.bat
@@ -1,8 +1,9 @@
 javac --release 11 src/domain/classes/*.java 
-javac --release 11 src/domain/controllers/*.java 
+javac --release 11 -cp .:../lib/json-20231013.jar src/domain/controllers/*.java 
 javac --release 11 -cp .;../lib/junit-4.13.2.jar src/drivers/*.java  
 javac --release 11 src/exceptions/*.java 
 javac --release 11 src/presentation/*.java 
+javac --release 11 src/data/*.java ;
 javac --release 11 src/Main.java
 
 move src\domain\classes\*.class ..\bin\class\src\domain\classes\ 
@@ -10,4 +11,5 @@ move src\domain\controllers\*.class ..\bin\class\src\domain\controllers\
 move src\drivers\*.class ..\bin\class\src\drivers\ 
 move src\exceptions\*.class ..\bin\class\src\exceptions\ 
 move src\presentation\*.class ..\bin\class\src\presentation\ 
+move src\data\*.class ..\bin\class\src\data\ 
 move src\Main.class ..\bin\class\src\ 
--- src/make.sh
+++ src/make.sh
@@ -1,7 +1,8 @@
 javac --release 11 src/domain/classes/*.java ;
-javac --release 11 src/domain/controllers/*.java ;
+javac --release 11 -cp .:../lib/json-20231013.jar src/domain/controllers/*.java ;
 javac --release 11 -cp .:../lib/junit-4.13.2.jar src/drivers/*.java ;
 javac --release 11 src/exceptions/*.java ;
+javac --release 11 src/data/*.java ;
 javac --release 11 src/presentation/*.java ;
 javac --release 11 src/Main.java ;
 
@@ -10,4 +11,5 @@ mv src/domain/controllers/*.class ../bin/class/src/domain/controllers/ ;
 mv src/drivers/*.class ../bin/class/src/drivers/ ;
 mv src/exceptions/*.class ../bin/class/src/exceptions/ ;
 mv src/presentation/*.class ../bin/class/src/presentation/ ;
+mv src/data/*.class ../bin/class/src/data/ ;
 mv src/Main.class ../bin/class/src/ ;
