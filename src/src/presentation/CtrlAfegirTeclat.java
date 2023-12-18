ebe8d1369818a11469f06a78e5baa36640a7a26a
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-12-18 23:27:34 +0100

    inici ctrlAfegirTeclat

--- src/src/presentation/CtrlAfegirTeclat.java
+++ src/src/presentation/CtrlAfegirTeclat.java
@@ -1,5 +1,33 @@
 package src.presentation;
 
-public class CtrlAfegirTeclat {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class CtrlAfegirTeclat {
+    private JLabel títol;
+    private JTextField nomTeclat;
+    private JList listAlfabets, generadors;
+    private String[] arryAlfabets;
+    private JButton cancelar, confirmar;
+    private JFrame vista;
+    private CtrlPresentacio ctrlPresentacio;
+    private JPanel PTítol, PSouth;
+
+    public CtrlAfegirTeclat() {
+        ctrlPresentacio = CtrlPresentacio.getInstance();
+        init();
+        addElementsFrame();
+    }
+
+    public void init() {
+
+    }
+
+    public void addElementsFrame() {
+        //vista.add
+    }
 }
