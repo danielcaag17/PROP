f9d7d2099f32e8da63688e6f6e481d318742e352
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-12-18 18:55:16 +0100

    Nova compilació fet i correcció make.bat

--- src/src/presentation/CtrlPantallaInici.java
+++ src/src/presentation/CtrlPantallaInici.java
@@ -68,6 +68,6 @@ public class CtrlPantallaInici {
 
     private void start() {
         vista.setVisible(false); // Diferent entre no fer visible i eliminar la pantalla 
-        ctrlPresentacio.canviVista("LlistaTeclats");
+        ctrlPresentacio.canviVista("LlistaAlfabets");
     }
 }
