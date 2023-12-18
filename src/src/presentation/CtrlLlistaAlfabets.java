d879ccbf854bab744be7687c72c09a9883c9430a
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-12-18 18:44:34 +0100

    panels afegits

--- src/src/presentation/CtrlLlistaAlfabets.java
+++ src/src/presentation/CtrlLlistaAlfabets.java
@@ -1,6 +1,9 @@
 package src.presentation;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -24,11 +27,42 @@ public class CtrlLlistaAlfabets {
         títol = Utils.initLabel("Llista Alfabets", "title");
 
         back = Utils.backButton();
+        back.addActionListener(e -> back());
+        afegir = new JButton();
+
+        PTítol = new JPanel();
+        PTítol.setPreferredSize(new Dimension(Utils.getScreenWidth(),Utils.getScreenHeight()/6));
+        PTítol.add(títol);
+
+        PSouth = new JPanel();
+        PSouth.setPreferredSize(new Dimension(Utils.getScreenWidth(),Utils.getScreenHeight()/6));
+        PSouth.add(back);
+
+        PAfegir = new JPanel();
+        PAfegir.setPreferredSize(new Dimension(Utils.getScreenWidth(),Utils.getScreenHeight()/6));
+        PAfegir.add(afegir);
+
+        PLlista = new JPanel();
+        PLlista.setLayout(new GridLayout());
+        // la mida és tota la restant de la pantall
+        
+
+        PCenter = new JPanel();
+        PCenter.setLayout(new BorderLayout());
+        PCenter.add(PAfegir, BorderLayout.NORTH);
+        PCenter.add(PLlista, BorderLayout.SOUTH);
 
         vista = Utils.initFrame();
     }
 
     private void addElementsFrame() {
+        vista.add(PTítol, BorderLayout.NORTH);
+        vista.add(PCenter, BorderLayout.CENTER);
+        vista.add(PSouth, BorderLayout.SOUTH);
+    }
 
+    private void back() {
+        // Eliminar la vista
+        ctrlPresentacio.canviVista("LlistaTeclats");
     }
 }
