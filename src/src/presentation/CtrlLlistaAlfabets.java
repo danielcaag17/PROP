1ce6083717f0e675faa1e907502fc0d97141379d
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-12-19 23:31:43 +0100

    Llista alfabets UI bastant completa

--- src/src/presentation/CtrlLlistaAlfabets.java
+++ src/src/presentation/CtrlLlistaAlfabets.java
@@ -1,21 +1,26 @@
 package src.presentation;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class CtrlLlistaAlfabets {
+    private CtrlPresentacio ctrlPresentacio;
     private JLabel títol;
     private JButton afegir, back; 
     private JFrame vista;
     private JPanel PTítol, PSouth, PCenter, PAfegir, PLlista;
 
     public CtrlLlistaAlfabets() {
+        ctrlPresentacio = CtrlPresentacio.getInstance();
         init();
         addElementsFrame();
     }
@@ -23,7 +28,7 @@ public class CtrlLlistaAlfabets {
     private void init() {
         títol = Utils.initLabel("Llista Alfabets", "title");
 
-        back = Utils.backButton();
+        back = Utils.imatgeButton("backArrow");
         back.addActionListener(e -> Utils.canviPantalla(vista, "LlistaTeclats"));
         
         afegir = Utils.addButton();
@@ -42,14 +47,30 @@ public class CtrlLlistaAlfabets {
         PAfegir.add(afegir);
 
         PLlista = new JPanel();
-        PLlista.setLayout(new GridLayout());
-        // la mida és tota la restant de la pantall
+        PLlista.setLayout(new BoxLayout(PLlista, BoxLayout.Y_AXIS));
+        String[] alfabets = ctrlPresentacio.getNomAlfabets();
+        for (int i = 0; i < alfabets.length; i++) {
+            JLabel label = Utils.initLabel(alfabets[i], "text");
+            label.setAlignmentX(Component.CENTER_ALIGNMENT);
+            JButton edit = Utils.imatgeButton("edit");
+            JButton delete = Utils.imatgeButton("delete");
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new FlowLayout());
+            panel.add(label);
+            panel.add(edit);
+            panel.add(delete);
+
+            PLlista.add(panel);
+        }
         
+        JScrollPane scrollPane = new JScrollPane(PLlista);
+        scrollPane.setBorder(null);     // No se que queda millor
 
         PCenter = new JPanel();
         PCenter.setLayout(new BorderLayout());
         PCenter.add(PAfegir, BorderLayout.NORTH);
-        PCenter.add(PLlista, BorderLayout.SOUTH);
+        PCenter.add(scrollPane, BorderLayout.CENTER);
 
         vista = Utils.initFrame();
     }
