f9abca2c1766e9ee3fa5694d60f089ff2411870e
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2023-12-20 23:27:37 +0100

    Pantalla informativa acabada menys UI

--- src/src/presentation/PantallaInformativa.java
+++ src/src/presentation/PantallaInformativa.java
@@ -9,16 +9,22 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class PantallaInformativa {
+    private JDialog dialog;
+
     public PantallaInformativa(JFrame pantalla, String title, String msg) {
-        JDialog dialog = new JDialog(pantalla, "NoEsPotCrearTeclat", true);
+        dialog = new JDialog(pantalla, "NoEsPotCrearTeclat", true);
         dialog.setLayout(new BorderLayout());
         JButton button = Utils.Button("OK", null);
+        button.addActionListener(e -> close());
         dialog.add(new JLabel(msg), BorderLayout.CENTER);
         dialog.add(button, BorderLayout.SOUTH);
         dialog.setSize(new Dimension(420,200));
         dialog.setLocationRelativeTo(pantalla);
         dialog.setVisible(true);
         dialog.setResizable(false);
-        
+    }
+
+    private void close(){
+        dialog.dispose();
     }
 }
