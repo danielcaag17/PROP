d33a4b2511b869e4f357d778d19ef3c110702f0b
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-12-20 23:03:33 +0100

    CtrlEliminar

--- src/src/presentation/CtrlEliminar.java
+++ src/src/presentation/CtrlEliminar.java
@@ -1,8 +1,66 @@
 package src.presentation;
 
+import javax.swing.JPopupMenu;
+
+import src.exceptions.*;
+
 public class CtrlEliminar {
-    public CtrlEliminar (String tipus, String missatge) {
-        //tipus == teclat or alfabet or layout
-        //missatge el que es vulgui
+    String tipus = "";
+    String clau = "";
+    CtrlPresentacio cp;
+    JPopupMenu popupEliminarMenu;
+
+    /**
+     * Constructora per defecte
+     *
+     * @param tipus indica el tipus d'instància que es vol eliminar
+     * @param clau indica la clau de l'instància que es vol eliminar
+     */
+    public CtrlEliminar (String tipus, String clau, String pantalla) {
+        this.tipus = tipus;
+        this.clau = clau;
+        cp = CtrlPresentacio.getInstance();
+        initPopup();
     }
+
+    /**
+     * Inicialitza el popup per a eliminar
+     */
+    public void initPopup() {
+        popupEliminarMenu = new JPopupMenu();
+        popupEliminarMenu.add("Eliminar");
+        popupEliminarMenu.add("Cancelar");
+    }
+
+    /**
+     * Elimina l'instància seleccionada
+     */
+    public void eliminar () {
+        switch (tipus) {
+            case "Teclat":
+                try {
+                    cp.esborrarTeclat(clau);
+                }
+                catch (Excepcions e) {
+                    cp.Excepcio(e.getTipus(), e.getMessage());;
+                }
+                break;
+            case "Alfabet":
+                try {
+                    cp.esborrarAlfabet(clau);
+                }
+                catch (Excepcions e) {
+                    cp.Excepcio(e.getTipus(), e.getMessage());;
+                }
+                break;
+            case "Layout":
+                try {
+                    cp.esborrarLayout(Integer.valueOf(clau));
+                }
+                catch (Excepcions e) {
+                    cp.Excepcio(e.getTipus(), e.getMessage());;
+                }
+                break;
+        }
+    }    
 }
