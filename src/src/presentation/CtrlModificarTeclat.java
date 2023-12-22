a5fd488d57801337be8c060371f0a49c4961a721
danielcaag <daniel.canizares@estudiantat.upc.edu>
2023-12-22 11:22:18 +0100

    modificar teclat implementat i acabat

--- src/src/presentation/CtrlModificarTeclat.java
+++ src/src/presentation/CtrlModificarTeclat.java
@@ -4,23 +4,52 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import src.exceptions.Excepcions;
 import src.exceptions.TeclatNoExisteix;
 
 public class CtrlModificarTeclat {
     private CtrlPresentacio ctrlPresentacio;                // instancia unica de ctrlPresentacio
     private JPanel PNorth, PCenter, PSouth;                 // panels per organitzar el layout de la vista
+    private JButton buttonOK, buttonHelp, cancelar, confirmar;
     private JFrame vista;                                   // vista on es mostra tota la informació de la pantalla
     private String teclat;                                  // nom del teclat a modificar
+    private Character primera, segona;                      // primera i segona lletra per a fer una modificació
+    private JButton buttonPrimer, buttonSegon;              // els dos botons seleccionats
+    private Map<Character, Character> canvis;
 
     public CtrlModificarTeclat(String elementAMostrar) {
         teclat = elementAMostrar;
         ctrlPresentacio = CtrlPresentacio.getInstance();
+        initElements();
         initPanels();
         addElementsFrame();
+    }
+
+    private void initElements() {
+        canvis = new HashMap<>();
+        primera = segona = Character.MIN_VALUE;             // character buit
+        buttonPrimer = buttonSegon = null;
+
+        buttonOK = Utils.Button("OK", null);
+        buttonOK.addActionListener(e -> parellaOK());
+
+        buttonHelp = Utils.Button("?", null);
+        buttonHelp.addActionListener(e -> help());
+
+        cancelar = Utils.Button("Cancelar", null);
+        cancelar.addActionListener(e -> cancelar());
+
+        confirmar = Utils.Button(null, "check");
+        confirmar.addActionListener(e -> confirmar());
     }
 
     private void initPanels() {
@@ -33,8 +62,8 @@ public class CtrlModificarTeclat {
         JPanel PTeclat = panelTeclat(teclat);
 
         JPanel PButtons = Utils.JPanel(new FlowLayout(FlowLayout.CENTER, 50, 10), null);
-        PButtons.add(Utils.Button("OK", null));
-        PButtons.add(Utils.Button("?", null));
+        PButtons.add(buttonOK);
+        PButtons.add(buttonHelp);
 
         PCenter = Utils.JPanel(new BorderLayout(), null);
         PCenter.add(PNom, BorderLayout.NORTH);
@@ -42,8 +71,8 @@ public class CtrlModificarTeclat {
         PCenter.add(PButtons, BorderLayout.SOUTH);
 
         PSouth = Utils.JPanel(new FlowLayout(FlowLayout.CENTER, 50, 10), new Dimension(Utils.getScreenWidth(),Utils.getScreenHeight()/6));
-        PSouth.add(Utils.Button(null, "cross"));
-        PSouth.add(Utils.Button(null, "check"));
+        PSouth.add(cancelar);
+        PSouth.add(confirmar);
     }
 
     private void addElementsFrame() {
@@ -71,7 +100,12 @@ public class CtrlModificarTeclat {
                 JPanel PRow = Utils.JPanel(new GridLayout(1, size), null);
                 for (int j = 0; j < distribucio[i].length; j++) {
                     String character = String.valueOf(distribucio[i][j]);
-                    if (! character.equals("-")) PRow.add(Utils.Button(character, null));
+                    if (! character.equals("-")) {
+                        JButton button = Utils.Button(character, null);
+                        char c = distribucio[i][j];
+                        button.addActionListener(e -> lletraSeleccionada(c, button));
+                        PRow.add(button);
+                    }
                 }
                 panel.add(PRow);
             }
@@ -79,5 +113,66 @@ public class CtrlModificarTeclat {
             ctrlPresentacio.Excepcio(vista, e.getTipus(), "Teclat " + teclat + " no exiteix");
         }
         return panel;
+    }
+
+    private void parellaOK() {
+        if (primera != Character.MIN_VALUE && segona != Character.MIN_VALUE) {
+            canvis.put(primera, segona);
+            ctrlPresentacio.Excepcio(vista, "OK", "Canvi modificat correctament");
+            String aux = buttonPrimer.getText();
+            buttonPrimer.setText(buttonSegon.getText());
+            buttonSegon.setText(aux);
+
+            primera = segona = Character.MIN_VALUE;
+            buttonPrimer.setEnabled(true);
+            buttonSegon.setEnabled(true);
+            buttonPrimer = buttonSegon = null;
+        }
+        else if (primera != Character.MIN_VALUE && segona == Character.MIN_VALUE) {
+            ctrlPresentacio.Excepcio(vista, "FaltaUnaLletra", "Falta seleccionar una lletra més per fer un canvi");
+        }
+        else {
+            ctrlPresentacio.Excepcio(vista, "FaltaSeleccionarDuesLletres", "Falta seleccionar dues lletres");
+        }
+    }
+
+    private void help() {
+        String msg = "Has de seleccionar dues lletres a intercanviar i clicar OK, un cop acabades totes les modificacions -> confirmar";
+        ctrlPresentacio.Excepcio(vista, "Ajuda", msg);
+    }
+
+    private void cancelar() {
+        Utils.canviPantalla(vista, "LlistaTeclats");
+    }
+
+    private void confirmar() {
+        try {
+            ctrlPresentacio.modificarTeclat(teclat, canvis);
+        } catch (Excepcions | IOException e) {
+            String msg = "";
+            String tipus = "";
+            if (e instanceof FileNotFoundException) {
+                msg = e.getMessage();
+                tipus = "FileNotFoundException";
+            }
+            else if (e instanceof Excepcions) {
+                msg = e.getMessage();
+                tipus = "FileNotFoundException";
+            }
+            ctrlPresentacio.Excepcio(vista, tipus, msg);
+        }
+        Utils.canviPantalla(vista, "LlistaTeclats");
+    }
+
+    private void lletraSeleccionada(char c, JButton button) {
+        if (segona == Character.MIN_VALUE) {
+            if (primera == Character.MIN_VALUE) primera = c;
+            else segona = c;
+            
+            button.setEnabled(false);
+            if (buttonPrimer == null) buttonPrimer = button;
+            else buttonSegon = button;
+        }
+        else ctrlPresentacio.Excepcio(vista, "JaDuesLletresSeleccionades", "Ja has seleccioant dues lletres");
     }
 }
