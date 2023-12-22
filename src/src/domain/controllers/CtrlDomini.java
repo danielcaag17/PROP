4c73bd1d394109224a302fcb79aca8c4b577a472
Pau Rambla <pau.rambla@estudiantat.upc.edu>
2023-12-22 11:25:10 +0100

    Refactor JSON parsing and object instantiation in CtrlDomini.java

--- src/src/domain/controllers/CtrlDomini.java
+++ src/src/domain/controllers/CtrlDomini.java
@@ -425,13 +425,13 @@ public class CtrlDomini {
         ArrayList<String> filesLayouts = ctrlPersistFile.getAll("layouts");
         for (String file : filesAlfabets) {
             String aRaw = ctrlPersistFile.getObject(file);
-            // TODO: extreure dades de aRaw del JSON i crear instància de Alfabet
-            String nom = "";
-            Map<Character, Double> character; 
-            Character[] abecedari;
-            double[][] frequencies;
+            JSONObject obj = new JSONObject(aRaw);
+            String nom = obj.getJSONObject("nom").getString("nom");
+            Map<Character, Double> character = jsonObjToMapCharDoub(obj.getJSONObject("characters"));
+            // Character[] abecedari = jsonArrToArray(obj.getJSONArray("abecedari"));
+            double[][] frequencies = jsonArrToDoubleMat(obj.getJSONArray("frequencies"));
             Alfabet a;
-            a = new Text(nom);
+            a = new Text(nom, character, frequencies);
             Alfabets.put(nom, a);
         }
         for (String file : filesLayouts) {
@@ -443,32 +443,52 @@ public class CtrlDomini {
         }
         for (String file : filesTeclats) {
             String tRaw = ctrlPersistFile.getObject(file);
-            // TODO: extreure dades de tRaw del JSON i crear instància de Teclat
             JSONObject obj = new JSONObject(tRaw);
             String nom = obj.getJSONObject("nom").getString("nom");
-            Teclat t = new Teclat(nom, null, null, null);
+            Map<Character, Integer> teclat = jsonObjToMapCharInt(obj.getJSONObject("teclat"));
+            char[][] distribucioCharacters = jsonArrToCharMat(obj.getJSONArray("distribucioCharacters"));
+            Teclat t = new Teclat(nom, teclat, distribucioCharacters);
             Teclats.put(nom, t);
         }
     }
-}
 
-/*
     // Funcio que retorna JSONArray a Matriu
     // Exemple:
     //           JSONArray matrixArray = jsonObject.getJSONArray("matrix");
     //           int[][] matrix = jsonArrToMat(matrixArray);
     //
-    private static int[][] jsonArrToMat(JSONArray in_mat) {
+    private char[][] jsonArrToCharMat(JSONArray mat) {
         // Convert the JSONArray to a 2D array (matrix)
-        int rows = in_mat.length();
-        int cols = in_mat.getJSONArray(0).length();
+        int rows = mat.length();
+        int cols = mat.getJSONArray(0).length();
 
-        int[][] res_mat = new int[rows][cols];
+        char[][] res_mat = new char[rows][cols];
 
         for (int i = 0; i < rows; i++) {
-            JSONArray row = in_mat.getJSONArray(i);
+            JSONArray row = mat.getJSONArray(i);
             for (int j = 0; j < cols; j++) {
-                res_mat[i][j] = row.getInt(j);
+                res_mat[i][j] = row.getString(j).charAt(0);
+            }
+        }
+        return res_mat;
+    }
+
+    // Funcio que retorna JSONArray a Matriu
+    // Exemple:
+    //           JSONArray matrixArray = jsonObject.getJSONArray("matrix");
+    //           int[][] matrix = jsonArrToMat(matrixArray);
+    //
+    private double[][] jsonArrToDoubleMat(JSONArray double_mat) {
+        // Convert the JSONArray to a 2D array (matrix)
+        int rows = double_mat.length();
+        int cols = double_mat.getJSONArray(0).length();
+
+        double[][] res_mat = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            JSONArray row = double_mat.getJSONArray(i);
+            for (int j = 0; j < cols; j++) {
+                res_mat[i][j] = row.getDouble(j);
             }
         }
         return res_mat;
@@ -479,7 +499,7 @@ public class CtrlDomini {
     //          JSONArray jsoncharArray = jsonObject.getJSONArray("char_array");
     //          Character[] new_arr = jsonArrToArray(jsoncharArray);
     //
-    private static Character[] jsonArrToArray(JSONArray json_arr) {
+    private Character[] jsonArrToArray(JSONArray json_arr) {
         int length = json_arr.length();
         Character[] res_arr = new Character[length];
         for (int i = 0; i < length; i++) res_arr[i] = json_arr.getString(i).charAt(0);
@@ -490,7 +510,7 @@ public class CtrlDomini {
     // Exemple:
     // Map<Character, Double> recoveredMap = jsonObjToMap(jsonObject.getJSONObject("mapCharDouble"));
     //
-    private static Map<Character, Double> jsonObjToMap(JSONObject json_obj) {
+    private Map<Character, Double> jsonObjToMapCharDoub(JSONObject json_obj) {
         Map<Character, Double> res_map = new HashMap<>();
         for (String key : json_obj.keySet()) {
             char charKey = key.charAt(0);
@@ -499,4 +519,18 @@ public class CtrlDomini {
         }
         return res_map;
     }
-*/
+
+    // Funcio que retorna JSONObject a Map<Character, Integer>
+    // Exemple:
+    // Map<Character, Double> recoveredMap = jsonObjToMap(jsonObject.getJSONObject("mapCharDouble"));
+    //
+    private Map<Character, Integer> jsonObjToMapCharInt(JSONObject json_obj) {
+        Map<Character, Integer> res_map = new HashMap<>();
+        for (String key : json_obj.keySet()) {
+            char charKey = key.charAt(0);
+            int value = json_obj.getInt(key);
+            res_map.put(charKey, value);
+        }
+        return res_map;
+    }
+}
